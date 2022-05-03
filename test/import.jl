f6de44eab7ce04fa4c834470169cecc21af8a50e
JosifFP <josif.figueroa@unb.ca>
2022-05-03 10:57:08 -0300

    commit

--- test/import.jl
+++ test/import.jl
@@ -1,6 +1,6 @@
 
 using PRATS, Reexport, XLSX
-using PRAS
+#using PRAS
 #using ContingencySolver
 
 networkfile = "test/data/RTS.raw"
@@ -9,5 +9,17 @@ studycase = [networkfile, loadfile]
 #data = ContingencySolver.build_data(studycase[1])
 
 sys = PRATS.SystemModel(loadfile)
-shortfalls, flows = PRAS.assess(sys, SequentialMonteCarlo(samples=100), Shortfall(), Flow())
-lole =  PRAS.EUE(shortfalls, "1")
+#@time shortfalls = PRATS.assess(sys, SequentialMonteCarlo(samples=2_500), Shortfall())
+
+simspec = SequentialMonteCarlo(samples=2000, threaded=false)
+
+resultspecs = (Shortfall(), Surplus(), Flow(), Utilization(),
+               ShortfallSamples(), SurplusSamples(),
+               FlowSamples(), UtilizationSamples(),
+               GeneratorAvailability())
+
+               @time shortfall, surplus, flow, utilization, shortfallsamples, surplussamples, flowsamples,
+               utilizationsamples, generatoravailability = assess(sys, simspec, resultspecs...)
+
+LOLE(shortfall)
+EUE(shortfall)
