571128d0b0606e5094e74c9c1b6c2252260377ae
JosifFP <josif.figueroa@unb.ca>
2022-05-11 16:38:32 -0300

    commit

--- examples.jl
+++ examples.jl
@@ -1,26 +1,32 @@
 using Distributions
 using PRATS
+using Base.Threads
 
 
 loadfile = "test/data/rts_Load.xlsx"
 system = PRATS.SystemModel(loadfile);
 simspec = SequentialMonteCarlo(samples=1,seed=1, threaded=false)
+method = simspec
 resultspecs = (Shortfall(), Surplus())
 
 #SequentialMonteCarlo.jl
-threads = 1
+threads = nthreads()
 sampleseeds = Channel{Int}(2*threads)
-results = resultchannel(method, resultspecs, threads)
+results = PRATS.resultchannel(method, resultspecs, threads)
+Threads.@spawn makeseeds(sampleseeds, method.nsamples)
 
-resultspecs = (Shortfall(), Surplus())
-
+if method.threaded
+    for _ in 1:threads
+        println(_)
+    #    @spawn assess(system, method, sampleseeds, results, resultspecs...)
+    end
+else
+    #assess(system, method, sampleseeds, results, resultspecs...)
+end
 
 function makeseeds(sampleseeds::Channel{Int}, nsamples::Int)
-
     for s in 1:nsamples
         put!(sampleseeds, s)
     end
-
     close(sampleseeds)
-
 end
