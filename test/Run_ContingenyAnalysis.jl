44767c676e82c735a8584248dbc19b86308590b8
Josif Figueroa <71727640+JosifFP@users.noreply.github.com>
2022-08-23 14:07:17 -0300

    commit

--- test/Run_ContingenyAnalysis.jl
+++ test/Run_ContingenyAnalysis.jl
@@ -2,7 +2,7 @@ using PRATS
 using PRATS.PRATSBase
 using PRATS.CompositeAdequacy
 import Memento; const _LOGGER = Memento.getlogger(@__MODULE__)
-import PowerModels, Ipopt, Juniper, BenchmarkTools, JuMP
+using PowerModels, Ipopt, Juniper, BenchmarkTools, JuMP
 using Test
 
 RawFile =  "C:/Users/jfiguero/Desktop/PRATS Input Data/RTS.raw"
@@ -28,7 +28,7 @@ systemstate = SystemState(system)
 #recorders = accumulator.(system, method, resultspecs)
 rng = PRATS.CompositeAdequacy.Philox4x((0, 0), 10)
 PRATS.CompositeAdequacy.seed!(rng, (method.seed, 1))  #using the same seed for entire period.
-N =8760
+N = 8760
 t = 1
 
 # system.loads.keys
@@ -40,8 +40,15 @@ t = 1
 # system.loads
 
 network_data =  PowerModels.parse_file(RawFile)
-network_data_mn = PowerModels.replicate(network_data, N)
+pf_result = PowerModels.compute_dc_pf(network_data)
+PowerModels.update_data!(network_data, pf_result["solution"])
+flow = PowerModels.calc_branch_flow_dc(network_data)
 
+for i in eachindex(network_data["branch"])
+    @test abs(flow["branch"][i]["pf"]) <= network_data["branch"][i]["rate_a"]
+end
+
+network_data_mn = PowerModels.replicate(network_data, N)
 
 for j in eachindex(1:N)
     for i in eachindex(system.loads.keys)
@@ -52,47 +59,59 @@ for j in eachindex(1:N)
 end
 
 optimizer = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol"=>1e-6, "print_level"=>0)
-#DCPPowerModel, #ACPPowerModel
-pf_result = PowerModels.run_mn_opf(network_data_mn, DCPPowerModel, optimizer)
-#PowerModels.un_mn_pf()
+pf_result = PowerModels.solve_mn_opf(network_data_mn, DCPPowerModel, optimizer)                 #DCPPowerModel, #ACPPowerModel
+@test pf_result["termination_status"] == LOCALLY_SOLVED
+
+# for j in eachindex(1:N)
+#     for (i, branch) in pf_result["solution"]["nw"][string(j)]["branch"]
+#         @test abs(branch["pf"]) <= network_data["branch"][i]["rate_a"]
+#     end
+# end
+
+PowerModels.update_data!(network_data_mn, pf_result["solution"])
+container = Dict{Int64, Any}()
+
+for j in eachindex(1:N)
+
+    setindex!(network_data_mn["nw"][string(j)], true, "per_unit")
+
+    for (i, branch) in pf_result["solution"]["nw"][string(j)]["branch"]
+
+        if branch["pf"] > network_data["branch"][string(i)]["rate_a"]
+            
+            Memento.info(_LOGGER, "Branch (f_bus,t_bus)=($(network_data["branch"][string(i)]["f_bus"]),$(network_data["branch"][string(i)]["t_bus"])) is overloaded by %$(
+                Float16(branch["pf"]*100/network_data["branch"][string(i)]["rate_a"])), MW=$(
+                Float16(branch["pf"])), rate_a = $(network_data["branch"][string(i)]["rate_a"]), key=$(
+                i), index=$(network_data["branch"][string(i)]["index"]), Hour=$(j).")
+            opf = PowerModels.solve_opf(network_data_mn["nw"][string(j)], DCPPowerModel, optimizer)
+            @test opf["termination_status"] == LOCALLY_SOLVED
+
+            pf_result["solution"]["nw"][string(j)]["branch"] = opf["solution"]["branch"]
+            @test branch["pf"]["pf"] <= network_data["branch"][string(i)]["rate_a"]
+
+            pf_result["solution"]["nw"][string(j)]["gen"] = opf["solution"]["gen"]
+            pf_result["solution"]["nw"][string(j)]["bus"] = opf["solution"]["bus"]
+        
+        end
+
+        flow = PowerModels.calc_branch_flow_dc(network_data_mn["nw"][string(j)])
+        setindex!(container, [abs.(Float16.(flow["branch"][string(k)]["pf"])) for k in eachindex(system.branches.keys)], j)
+    end
+end
+
+
+#PowerModels.run_mn_pf()
 #PowerModels.run_mn_opf_strg()
 #PowerModels.run_mn_opf()
 #TESTLOG = Memento.getlogger(PowerModels)
 #@test_throws(TESTLOG, ErrorException, PowerModels.correct_voltage_angle_differences!(network_data))
 #@test_throws(TESTLOG, ErrorException, PowerModels.calc_connected_components(network_data))
 
-@test pf_result["termination_status"] == LOCALLY_SOLVED
-PowerModels.update_data!(network_data_mn, pf_result["solution"])
-container = Dict{Int64, Any}()
-for j in eachindex(1:24)
-    setindex!(network_data_mn["nw"][string(j)], true, "per_unit")
-    flow = PowerModels.calc_branch_flow_dc(network_data_mn["nw"][string(j)])
-    setindex!(container, [(Float16.(flow["branch"][string(k)]["pf"]),Float16.(flow["branch"][string(k)]["pt"])) for k in eachindex(system.branches.keys)], j)
-end
 
 container_data = [container[i] for i in keys(container)]
 @show container_data[1]
 @show container_data[2]
 
-pf_result
-pf_result["solution"]["nw"]["1"]["branch"]
-network_data["branch"]["1"]["rate_a"]
-
-
-for j in eachindex(1:24)
-    data = pf_result["solution"]["nw"]["1"]["branch"]
-
-    for (i, branch) in data
-        @test data[i]["pf"] <= network_data["branch"][string(i)]["rate_a"]
-        println(i)
-        #@show isapprox(data[i]["pf"],network_data["branch"][string(i)]["rate_a"], atol = 1e-1)
-        println(data[i]["pf"])
-        println(network_data["branch"][string(i)]["rate_a"])
-    #     if isapprox(abs(data[i]["pf"],network_data["branch"][string(i)]["rate_a"], atol = 1e-1)==true
-    #         Memento.info(_LOGGER, "rate_a limit was hit on branch ID = $(ref[:branch][i]["source_id"]) at $(result["solution"]["branch"][i]["pf"]/ref[:baseMVA]))")
-    #     end
-    end
-end
 
 #system.network
 #data = PRATSBase.conversion_to_pm_data(system.network)
