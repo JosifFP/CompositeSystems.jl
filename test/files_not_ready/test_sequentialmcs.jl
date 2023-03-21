25611bc4321b01f0209023ade094061db1209417
Josif Figueroa <71727640+JosifFP@users.noreply.github.com>
2023-03-21 17:26:28 -0300

    Update test_sequentialmcs.jl

--- test/files_not_ready/test_sequentialmcs.jl
+++ test/files_not_ready/test_sequentialmcs.jl
@@ -14,7 +14,7 @@ resultspecs = (CompositeAdequacy.Shortfall(), CompositeAdequacy.GeneratorAvailab
 settings = CompositeSystems.Settings(
     gurobi_optimizer_3,
     jump_modelmode = JuMP.AUTOMATIC,
-    powermodel_formulation = OPF.DCPPowerModel,
+    powermodel_formulation = OPF.DCMPPowerModel,
     select_largest_splitnetwork = false,
     deactivate_isolated_bus_gens_stors = true,
     min_generators_off = 0,
@@ -22,18 +22,17 @@ settings = CompositeSystems.Settings(
     count_samples = false
 )
 
-timeseriesfile = "test/data/RBTS/Loads_system.xlsx"
-rawfile = "test/data/RBTS/Base/RBTS_AC.m"
-Base_reliabilityfile = "test/data/RBTS/Base/R_RBTS.m"
+timeseriesfile = "test/data/SMCS/RTS_79_A/Loads_system.xlsx"
+rawfile = "test/data/SMCS/RTS_79_A/RTS_AC_HIGH.m"
+Base_reliabilityfile = "test/data/SMCS/RTS_79_A/R_RTS.m"
 
-method = CompositeAdequacy.SequentialMCS(samples=7500, seed=100, threaded=true)
+method = CompositeAdequacy.SequentialMCS(samples=1000, seed=100, threaded=true)
 system = BaseModule.SystemModel(rawfile, Base_reliabilityfile, timeseriesfile)
 @time shortfall,_ = CompositeSystems.assess(system, method, settings, resultspecs...)
 CompositeSystems.print_results(system, shortfall)
 
 
 #run_mcs(method, resultspecs)
-
 
 function run_mcs(method, resultspecs)
 end
@@ -51,51 +50,3 @@ CompositeAdequacy.val.(CompositeSystems.EENS.(shortfall, system.buses.keys))
 CompositeAdequacy.stderror.(CompositeSystems.EENS.(shortfall, system.buses.keys))
 CompositeAdequacy.val.(CompositeSystems.EENS.(shortfall))
 CompositeAdequacy.stderror.(CompositeSystems.EENS.(shortfall))
-
-
-
-
-
-
-
-
-
-
-
-
-
-settings = CompositeSystems.Settings(
-    gurobi_optimizer_3,
-    jump_modelmode = JuMP.AUTOMATIC,
-    powermodel_formulation = OPF.DCPPowerModel,
-    select_largest_splitnetwork = false,
-    deactivate_isolated_bus_gens_stors = false,
-    min_generators_off = 1,
-    set_string_names_on_creation = false,
-    count_samples = false
-)
-
-timeseriesfile = "test/data/RBTS/Loads_system.xlsx"
-rawfile = "test/data/RBTS/Base/RBTS_AC.m"
-Base_reliabilityfile = "test/data/RBTS/Base/R_RBTS.m"
-system = BaseModule.SystemModel(rawfile, Base_reliabilityfile, timeseriesfile)
-@time shortfall,_ = CompositeSystems.assess(system, method, settings, resultspecs...)
-CompositeAdequacy.val.(CompositeSystems.EDLC.(shortfall, system.buses.keys))
-CompositeAdequacy.stderror.(CompositeSystems.EDLC.(shortfall, system.buses.keys))
-CompositeAdequacy.val.(CompositeSystems.EDLC.(shortfall))
-CompositeAdequacy.stderror.(CompositeSystems.EDLC.(shortfall))
-
-CompositeAdequacy.val.(CompositeSystems.EENS.(shortfall, system.buses.keys))
-CompositeAdequacy.stderror.(CompositeSystems.EENS.(shortfall, system.buses.keys))
-CompositeAdequacy.val.(CompositeSystems.EENS.(shortfall))
-CompositeAdequacy.stderror.(CompositeSystems.EENS.(shortfall))
-
-@testset "Sequential MCS, 1000 samples, RBTS" begin
-    timeseriesfile = "test/data/RBTS/Loads_system.xlsx"
-    rawfile = "test/data/RBTS/Base/RBTS_AC.m"
-    Base_reliabilityfile = "test/data/RBTS/Base/R_RBTS.m"
-    system = BaseModule.SystemModel(rawfile, Base_reliabilityfile, timeseriesfile)
-    @time shortfall,_ = CompositeSystems.assess(system, method, settings, resultspecs...)
-    @test isapprox(CompositeAdequacy.val.(CompositeSystems.EDLC.(shortfall, system.buses.keys)), [0.0, 0.0, 1.182, 0.0, 0.002, 10.357]; atol = 1e-3)
-    @test isapprox(CompositeAdequacy.val.(CompositeSystems.EENS.(shortfall, system.buses.keys)), [0.0, 0.0, 10.682, 0.0, 0.0194, 127.212]; atol = 1e-3)
-end
