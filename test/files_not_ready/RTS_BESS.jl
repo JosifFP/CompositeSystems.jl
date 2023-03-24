1b36e319219c7ad48328ed156cd327f86e73e493
Josif Figueroa <71727640+JosifFP@users.noreply.github.com>
2023-03-24 00:35:53 -0300

    Update RTS_BESS.jl

--- test/files_not_ready/RTS_BESS.jl
+++ test/files_not_ready/RTS_BESS.jl
@@ -22,7 +22,7 @@ timeseriesfile = "test/data/RTS/Loads_system.xlsx"
 rawfile = "test/data/others/Storage/RTS_strg.m"
 Base_reliabilityfile = "test/data/others/Storage/R_RTS_strg.m"
 resultspecs = (Shortfall(), BranchAvailability())
-method = SequentialMCS(samples=5000, seed=100, threaded=true)
+method = SequentialMCS(samples=2000, seed=100, threaded=true)
 system = BaseModule.SystemModel(rawfile, Base_reliabilityfile, timeseriesfile)
 
 for bus in 1:1:5
@@ -30,81 +30,16 @@ for bus in 1:1:5
 end
 
 function run_mcs(system, method, settings, resultspecs, bus::Int)
-    system.storages.buses[1] = bus
-    system.storages.charge_rating[1] = 0.25
-    system.storages.discharge_rating[1] = 0.25
-    system.storages.thermal_rating[1] = 0.25
-    for i in 0.25:0.25:1.5
-    #for i in 0.5:0.5:4.0
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.buses[1] = bus
-    system.storages.charge_rating[1] = 0.50
-    system.storages.discharge_rating[1] = 0.50
-    system.storages.thermal_rating[1] = 0.50
-    for i in 0.25:0.25:1.5
-    #for i in 0.5:0.5:4.0
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.buses[1] = bus
-    system.storages.charge_rating[1] = 0.75
-    system.storages.discharge_rating[1] = 0.75
-    system.storages.thermal_rating[1] = 0.75
-    for i in 0.25:0.25:1.5
-    #for i in 0.5:0.5:4.0
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.charge_rating[1] = 1.0
-    system.storages.discharge_rating[1] = 1.0
-    system.storages.thermal_rating[1] = 1.0
-    for i in 0.25:0.25:1.5
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.charge_rating[1] = 1.25
-    system.storages.discharge_rating[1] = 1.25
-    system.storages.thermal_rating[1] = 1.25
-    for i in 0.25:0.25:1.5
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.charge_rating[1] = 1.5
-    system.storages.discharge_rating[1] = 1.5
-    system.storages.thermal_rating[1] = 1.5
-    for i in 0.25:0.25:1.5
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.charge_rating[1] = 1.75
-    system.storages.discharge_rating[1] = 1.75
-    system.storages.thermal_rating[1] = 1.75
-    for i in 0.25:0.25:1.5
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
-    end
-
-    system.storages.charge_rating[1] = 2.0
-    system.storages.discharge_rating[1] = 2.0
-    system.storages.thermal_rating[1] = 2.0
-    for i in 0.25:0.25:1.5
-        system.storages.energy_rating[1] = i
-        shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
-        CompositeAdequacy.print_results(system, shortfall)
+    for j in 0.25:0.25:3.0
+        system.storages.buses[1] = bus
+        system.storages.charge_rating[1] = j
+        system.storages.discharge_rating[1] = j
+        system.storages.thermal_rating[1] = j
+        for i in 0.25:0.25:3.0
+            system.storages.energy_rating[1] = i
+            shortfall, _ = CompositeSystems.assess(system, method, settings, resultspecs...)
+            CompositeAdequacy.print_results(system, shortfall)
+            println("Bus: $(bus) power_rating: $(j), energy_rating: $(i)")
+        end
     end
 end
